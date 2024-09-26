f7969904a47d90f70dcc353a7893e9921d5aef31
RyanM3 <ryangmurray3@gmail.com>
2024-09-26 00:45:09 -0500

    changed index, created unit html, unit3 done

---
+++ script3.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Your future interactive scripts can be added here.
+    console.log("Page Loaded and Ready");
+});
+document.addEventListener("DOMContentLoaded", () => {
+    // Ready for any interactive script or additional logic
+    console.log("Page Loaded Successfully");
+});
